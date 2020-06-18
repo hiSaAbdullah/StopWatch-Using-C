e831a1a410415fc9ceef4a138bd8a314e3e9e9d9
SAabdullah31 <cse.asabdullah@gmail.com>
2020-06-18 17:47:48 +0600

    Stop Watch making using C program

---
+++ stopwatch.c
@@ -0,0 +1,48 @@
+// Stop Watch in C programming
+
+#include<stdio.h>
+#define CYCLE 5
+
+#include<unistd.h>
+#define CYCLE 5
+
+#ifdef _WIN32
+#include<conio.h>
+#define sleep(x) Sleep(x*1000)
+
+#else
+#define clrscr()printf("\e[1;1H\e[2J");
+#include<unistd.h>>
+#endif
+int main()
+{
+    int hour, minute, second;
+    printf("Enter Hour, Minute, Second: ");
+    scanf("%d%d%d",&hour,&minute,&second);
+    clrscr();
+        int h=0,m=0,s=0;
+
+    while(1){
+
+        printf("%.2d:%.2d:%.2d\n",h,m,s);
+
+        if(h==hour && m==minute && s==second){
+            break;
+        }
+        else{
+            clrscr();
+        }
+        s++;
+        sleep(1);
+        if(s==CYCLE){
+            m++;
+            s=0;
+        }
+        if(m==CYCLE){
+            h++;
+            m=0;
+        }
+    }
+    return 0;
+
+}
